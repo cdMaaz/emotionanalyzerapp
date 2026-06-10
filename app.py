ded7134779bd770230a3ae031eb08f893d2b1224
Muhammad Maaz <nomanmaaz446@gmail.com>
2026-06-10 08:57:21 +0500

    Integrate model prediction for emotion analysis

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import streamlit as st
 import requests
+import joblib
+model = joblib.load('pipeline.pickle')
 
 st.title("Emotion Analyzer")
 text = st.text_input("Analyze ANYONES emotion by entering their text here:")
 
 if st.button("Analyze"):
     if text:
-        response = requests.post("http://127.0.0.1:8000/predict/", json={"text": text})
-        result = response.json()
-        st.success(f"Predicted Emotion: {result['emotion']}")
+        prediction = model.predict([text])[0]
+        st.success(f"Emotion:{prediction.upper()}")
     else:
         st.warning("Please enter some text to analyze.")
